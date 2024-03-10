757304ac6ca002dd0a07c1edf1243c202cabb4ca
Art San <artsan.mcc@gmail.com>
2024-03-10 23:27:55 +0700

    ok

--- src/data/stores/useToDoStore.ts
+++ src/data/stores/useToDoStore.ts
@@ -1,80 +1,6 @@
-// import create from 'zustand'
-// import { generateId } from '../helpers'
-// import { persist, devtools } from 'zustand/middleware'
-// import { createStore } from './createStore'
+import create from 'zustand'
 
-// interface Task {
-//   id: string
-//   title: string
-//   createdAt: number
-// }
-// interface ToDoStore {
-//   tasks: Task[]
-//   tasksDone: Task[]
-//   createTask: (title: string) => void
-//   updateTask: (id: string, title: string) => void
-//   removeTask: (id: string) => void
-//   createTaskDone: () => void
-//   deleteEverything: () => void
-// }
-
-// export const useToDoStore = createStore<ToDoStore>(
-//   (set, get) => ({
-//     tasks: [],
-//     tasksDone: [],
-//     createTask: (title) => {
-//       const { tasks } = get()
-//       console.log('set', set)
-
-//       const newTask = {
-//         id: generateId(),
-//         title,
-//         createdAt: Date.now()
-//       }
-
-//       set({
-//         tasks: [newTask].concat(tasks)
-//       })
-//     },
-//     updateTask: (id: string, title: string) => {
-//       const { tasks } = get()
-//       set({
-//         tasks: tasks.map((task) => ({
-//           ...task,
-//           title: task.id === id ? title : task.title
-//         }))
-//       })
-//     },
-//     removeTask: (id: string) => {
-//       const { tasks, tasksDone } = get()
-//       set({
-//         tasks: tasks.filter((task) => task.id !== id),
-//         tasksDone: [...tasksDone].concat(tasks.filter((task) => task.id === id))
-//       })
-//     },
-//     createTaskDone: () => {
-//       const { tasksDone } = get()
-//       const newTask = {
-//         id: generateId(),
-//         title: 'Fake repeated title',
-//         createdAt: Date.now()
-//       }
-
-//       set({
-//         tasksDone: [newTask].concat(tasksDone)
-//       })
-//     },
-//     deleteEverything: () => {
-//       set({}, true) // Грохает весь стор
-//     }
-//   }),
-//   'Tasks-Test'
-// )
-
-// Работает хранилище и Редакс-тулс только TS ругается
 import { generateId } from '../helpers'
-import { persist, devtools } from 'zustand/middleware'
-import { createStore } from './createStore'
 
 interface Task {
   id: string
@@ -91,60 +17,76 @@ interface ToDoStore {
   deleteEverything: () => void
 }
 
-export const useToDoStore = createStore<ToDoStore>(
-  persist(
-    (set, get) => ({
-      tasks: [],
-      tasksDone: [],
-      createTask: (title) => {
-        const { tasks } = get()
-        console.log('set', set)
+export const useToDoStore = create<ToDoStore>((set, get) => ({
+  tasks: [],
+  tasksDone: [],
+  createTask: (title) => {
+    const { tasks } = get()
+    const newTask = {
+      id: generateId(),
+      title,
+      createdAt: Date.now()
+    }
 
-        const newTask = {
-          id: generateId(),
-          title,
-          createdAt: Date.now()
-        }
+    set({
+      tasks: [newTask].concat(tasks)
+    })
+  },
+  updateTask: (id: string, title: string) => {
+    const { tasks } = get()
+    set({
+      tasks: tasks.map((task) => ({
+        ...task,
+        title: task.id === id ? title : task.title
+      }))
+    })
+  },
+  removeTask: (id: string) => {
+    const { tasks, tasksDone } = get()
+    set({
+      tasks: tasks.filter((task) => task.id !== id),
+      tasksDone: [...tasksDone].concat(tasks.filter((task) => task.id === id))
+    })
+  },
+  createTaskDone: () => {
+    const { tasksDone } = get()
+    const newTask = {
+      id: generateId(),
+      title: 'Fake repeated title',
+      createdAt: Date.now()
+    }
 
-        set({
-          tasks: [newTask].concat(tasks)
-        })
-      },
-      updateTask: (id: string, title: string) => {
-        const { tasks } = get()
-        set({
-          tasks: tasks.map((task) => ({
-            ...task,
-            title: task.id === id ? title : task.title
-          }))
-        })
-      },
-      removeTask: (id: string) => {
-        const { tasks, tasksDone } = get()
-        set({
-          tasks: tasks.filter((task) => task.id !== id),
-          tasksDone: [...tasksDone].concat(
-            tasks.filter((task) => task.id === id)
-          )
-        })
-      },
-      createTaskDone: () => {
-        const { tasksDone } = get()
-        const newTask = {
-          id: generateId(),
-          title: 'Fake repeated title',
-          createdAt: Date.now()
-        }
+    set({
+      tasksDone: [newTask].concat(tasksDone)
+    })
+  },
+  deleteEverything: () => {
+    set({}, true)
+  }
+}))
 
-        set({
-          tasksDone: [newTask].concat(tasksDone)
-        })
-      },
-      deleteEverything: () => {
-        set({}, true)
-      }
-    }),
-    { name: 'MyStore' }
-  ),
-  'Tasks-Test-test'
-)
+// const unsub2 = useToDoStore.subscribe(state => state.tasks, console.log)
+
+// function isToDoStore(object: any): object is ToDoStore {
+//     return 'tasks' in object;
+// }
+
+// const localStorageUpdate = <T extends State>(config: StateCreator<T>): StateCreator<T> => (set, get, api) => config((nextState, ...args) => {
+//     if (isToDoStore(nextState)) {
+//         window.localStorage.setItem('tasks', JSON.stringify(
+//             nextState.tasks
+//         ));
+//     }
+//     set(nextState, ...args);
+// }, get, api);
+
+// const getCurrentState = () => {
+//     try {
+//         const currentState = (JSON.parse(window.localStorage.getItem('tasks') || '[]')) as Task[];
+//         return currentState;
+//     } catch(err) {
+//         window.localStorage.setItem('tasks', '[]');
+//     }
+
+//     return [];
+// }
